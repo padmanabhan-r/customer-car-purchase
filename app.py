733aecb0750e37bd5b28a2119a7d133151265f7a
Padmanabhan <53633680+padmanabhan-rajendrakumar@users.noreply.github.com>
2021-01-03 16:31:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import numpy as np
 from flask import Flask, request, render_template
-import tensorflow as tf
-physical_devices = tf.config.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
+#import tensorflow as tf
+#physical_devices = tf.config.list_physical_devices('GPU')
+#tf.config.experimental.set_memory_growth(physical_devices[0], True)
 from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
